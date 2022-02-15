ab0b4bf6b9cf46ca76f99af4105f5afd6298a512
freddiefreddy <mwendafredrick31@gmail.com>
2022-02-15 17:00:42 +0300

    'uie'

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
     return(
         <section id="about">
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow md:w-3/4 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <div className="lg:flex-grow sm:pr-16 md:w-3/4 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         Hello, my Name is Fredrick Mwenda
                         <br className="hidden lg:inline-block"/> A Passionate Software Developer
@@ -17,22 +17,15 @@ function About() {
                         societal problems in a technological way. Reachout, to turn that bright idea into a real world project.
                     </p>
                     <div className="flex justify-center">
-                        {/* /Use Linking to send  email and  */}
-                        <a href="tel:+254-713-723-353" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                        
+                        <a href="tel:+254-713-723-353" className=" text-white bg-green-500 border-0 py-2 px-2 focus:outline-none hover:bg-green-600 rounded text-lg sm:w-1/2">
                             +254713723353
                         </a>
 
-                        <a href="mailto:mwendafredrick31@gmail.com" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                        <a href="mailto:mwendafredrick31@gmail.com" className="ml-4 text-gray-400 bg-gray-800 border-0 py-2 px-2 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg ">
                             mwendafredrick31@gmail.com
                         </a>
-                        {/* <Button className=""  onPress={() => Linking.openURL(`tel:+254-713-723-353`) }
-                            title="0713723353" 
-                        /> */}
-
                         
-                        {/* <Button className=""  onPress={() => Linking.openURL('mailto:mwendafredrick@gmail.com') }
-                            title="mwendafredrick@gmail.com" 
-                        /> */}
                     </div>
                 </div>
 
